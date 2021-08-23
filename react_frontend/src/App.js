b9f88a94045ab78b9d868d37ac13ee47cad169be
xzhang49 <xzhanguh@gmail.com>
2021-08-23 16:10:24 -0500

    React application

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import ListProductComponent from './components/ListProductComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListProductComponent />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListProductComponent />
+      </div>
+      <FooterComponent />
     </div>
+    
   );
 }
 
