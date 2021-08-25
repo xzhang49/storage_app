fc8b606bacc26f11923ffb2c42a0a9fccc3ecee4
xzhang49 <xzhanguh@gmail.com>
2021-08-25 14:32:58 -0500

    React application

--- react_frontend/src/components/UpdateProductComponent.jsx
+++ react_frontend/src/components/UpdateProductComponent.jsx
@@ -63,39 +63,38 @@ class UpdateProductComponent extends Component {
             <div>
                 <br></br>
                    <div className = "container">
-                        <div className = "row">
+                        <div className="row">
                             <div className = "card col-md-6 offset-md-3 offset-md-3">
                                 <h3 className="text-center">Update Product</h3>
                                 <div className = "card-body">
                                     <form>
-                                        <div className = "form-group">
+                                        <div className="form-group">
                                             <label> Product Name: </label>
                                             <input placeholder="Product Name" name="productName" className="form-control" 
                                                 value={this.state.productName} onChange={this.changeProductNameHandler}/>
                                         </div>
-                                        <div className = "form-group">
+                                        <div className="form-group">
                                             <label> Sku Number: </label>
                                             <input placeholder="Sku Number" name="skuNumber" className="form-control" 
                                                 value={this.state.skuNumber} onChange={this.changeSkuNumberHandler}/>
                                         </div>
-                                        <div className = "form-group">
+                                        <div className="form-group">
                                             <label> Quantity: </label>
                                             <input placeholder="Quantity" name="quantity" className="form-control" 
                                                 value={this.state.quantity} onChange={this.changeQuantityHandler}/>
                                         </div>
-                                        <div className = "form-group">
+                                        <div className="form-group">
                                             <label> Location: </label>
                                             <input placeholder="Location" name="location" className="form-control" 
                                                 value={this.state.location} onChange={this.changeLocationHandler}/>
                                         </div>
-
+                                      
                                         <button className="btn btn-success" onClick={this.updateProduct}>Save</button>
                                         <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
-
                    </div>
             </div>
         )
